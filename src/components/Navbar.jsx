caa431b4aa26b2aaa6fd286de3cab5c795460c1b
ddhorstman <davidzilla27@gmail.com>
2020-03-18 20:39:11 -0400

    Test hook in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import useLocalStorage from "../hooks/useLocalStorage";
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useLocalStorage('darkMode', false );
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,11 +1,12 @@
 import React from "react";
 export default function useLocalStorage(key, initialValue) {
-    if (!initialValue) initialValue = "";
+    initialValue = initialValue ?? "";
     if (typeof initialValue !== "string") initialValue = JSON.stringify(initialValue);
-    const [value, setValue] = React.useState(() => JSON.parse(window.localStorage?.getItem(key)||initialValue));
+    const [value, setValue] = React.useState(() => JSON.parse(window.localStorage.getItem(key) ?? initialValue));
     const setValueLocalStorage = (newValue) => {
+        console.log("set value to",newValue);
         setValue(newValue);
-        window.localStorage?.setItem(key, JSON.stringify(newValue));
+        window.localStorage.setItem(key, JSON.stringify(newValue));
     };
     return [value, setValueLocalStorage];
 }
